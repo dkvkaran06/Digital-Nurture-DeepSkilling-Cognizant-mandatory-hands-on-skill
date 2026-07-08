48749190fb9807de4b0d50124293a572be41189e
dkvkaran06 <dkvkaran2004@gmail.com>
2026-07-08 15:37:14 +0530

    Convert Spring beans to annotations

--- Week 3 Spring Core and Maven,Spring Data JPA with SpringBoot, Hibernate/src/main/java/com/library/service/BookService.java
+++ Week 3 Spring Core and Maven,Spring Data JPA with SpringBoot, Hibernate/src/main/java/com/library/service/BookService.java
@@ -1,14 +1,15 @@
 package com.library.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.library.repository.BookRepository;
 
+@Service
 public class BookService {
 
+    @Autowired
     private BookRepository bookRepository;
-
-    public void setBookRepository(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     public void showBook() {
         System.out.println("Book Service");
