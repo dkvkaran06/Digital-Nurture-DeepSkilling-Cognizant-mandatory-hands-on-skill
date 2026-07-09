d2dfda2f4082b305307e671a1ffb1a5facd92e51
dkvkaran06 <dkvkaran2004@gmail.com>
2026-07-09 09:50:35 +0530

    Complete Week 3 Spring AOP exercises

--- Week 3 Spring Core and Maven,Spring Data JPA with SpringBoot, Hibernate/src/main/java/com/library/service/BookService.java
+++ Week 3 Spring Core and Maven,Spring Data JPA with SpringBoot, Hibernate/src/main/java/com/library/service/BookService.java
@@ -6,20 +6,16 @@ public class BookService {
 
     private BookRepository bookRepository;
 
-    // Constructor Injection
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-        System.out.println("Constructor Injection Executed");
-    }
-
-    // Setter Injection
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
-        System.out.println("Setter Injection Executed");
     }
 
     public void showBook() {
+
         System.out.println("Book Service");
+
         bookRepository.displayBook();
+
     }
+
 }
