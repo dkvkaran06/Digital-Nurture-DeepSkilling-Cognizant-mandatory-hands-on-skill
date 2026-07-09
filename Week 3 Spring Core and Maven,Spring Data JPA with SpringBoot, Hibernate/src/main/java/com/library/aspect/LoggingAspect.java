dbf3efec5fe825185fe5860016da1720d4cd4e38
dkvkaran06 <dkvkaran2004@gmail.com>
2026-07-09 09:55:00 +0530

    Exercise 3 - Logging with Spring AOP

--- Week 3 Spring Core and Maven,Spring Data JPA with SpringBoot, Hibernate/src/main/java/com/library/aspect/LoggingAspect.java
+++ Week 3 Spring Core and Maven,Spring Data JPA with SpringBoot, Hibernate/src/main/java/com/library/aspect/LoggingAspect.java
@@ -1,20 +1,25 @@
 package com.library.aspect;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(* com.library.service.*.*(..))")
-    public void beforeMethod() {
-        System.out.println("===== Before Method Execution =====");
-    }
+    @Around("execution(* com.library.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 
-    @After("execution(* com.library.service.*.*(..))")
-    public void afterMethod() {
-        System.out.println("===== After Method Execution =====");
+        long start = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println(joinPoint.getSignature().getName()
+                + " executed in " + (end - start) + " ms");
+
+        return result;
     }
 
 }
